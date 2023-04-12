7c3090295cfefc995822d512e4df800629382c3e
maung-htay <ukyishwejp@gmail.com>
2023-04-12 13:57:04 +0900

    test case for pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+from .views import HomePageVeiw
+
+class HomePageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+        
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+        
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200) 
+    
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "home.html")      
+    
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "Home")
+        
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Hi there!")    
+        
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageVeiw.as_view().__name__)    
