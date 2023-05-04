f4eb329da42889b4f717531c4fe7dfa0a98819b8
maung-htay <ukyishwejp@gmail.com>
2023-05-04 15:14:04 +0900

    update view.py:

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import (LoginRequiredMixin,    PermissionRequiredMixin)
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
 from django.views.generic import ListView, DetailView
 
 from .models import Book
